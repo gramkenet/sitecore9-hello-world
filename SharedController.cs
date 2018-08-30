3626065752177cdaa360ca5f4be9665c7ef5030e
Tyler Gramke <tyler@gramkegraphics.com>
2018-08-29 22:11:29 -0500

    Added model, views, controller, and static resources

---
+++ SharedController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SitecoreTest.Models;
+
+namespace SitecoreTest.Controllers
+{
+    public class SharedController : Controller
+    {
+        // GET: Shared
+        public ActionResult SharedMVCInnerRendering()
+        {
+			var model = new SharedViewModel
+			{
+				Year = Sitecore.DateUtil.ToServerTime(DateTime.UtcNow).Year,
+			};
+
+			return View(model);
+        }
+    }
+}
---
+++ SharedViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreTest.Models
+{
+	public class SharedViewModel
+	{
+		public int Year { get; set; }
+	}
+}
